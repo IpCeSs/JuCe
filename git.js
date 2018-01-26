c6a4eee940387dd65698f4a8bc69089e5051a8b5
darky74 <julien.fourdachon@gmail.com>
2018-01-26 13:45:07 +0100

    request eme

--- git.js
+++ git.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         console.log('dd');
 
  test
-        $('#bouton').after('<div>pull request 2eme session</div>');
+        $('#bouton').after('<div>pull request 3eme session</div>');
 
         
     })
