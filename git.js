adbed499412adcabbe6a1d1c7b59f82a7a470eb7
Cécilia <ceciliapigeolet@gmail.com>
2018-01-26 13:00:11 +0100

    cess

--- git.js
+++ git.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('#bouton').click(function(){
         console.log('dd');
 
-        $('#bouton').after('<div>yeahi </div>');
+        $('#bouton').after('<div>plus que 4heures!!!</div>');
     })
 
 });
