2a829e9d5fd7bd2376028ed1ab74683d3cade588
darky74 <julien.fourdachon@gmail.com>
2018-01-26 13:58:07 +0100

    push_test

--- git.js
+++ git.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     $('#bouton').click(function(){
         console.log('dd');
 
- test
-        $('#bouton').after('<div>pull request brache test</div>');
+ 
+        $('#bouton').after('<div>ca push depuis la brache test</div>');
 
         
     })
